c999f8fb638adefe21ef9a0a6476d8dea4d09ff2
PedroHN <pedronasci009@gmail.com>
2024-01-28 17:20:50 -0300

    Erro de Save nas Cédulas Pt.1
    Quando as variáveis eram salvas nas suas respectivas cédulas na planilha, eram todas armazenadas no tipo str, mesmo sendo floats, por isso o Excel tinha uma incompatibilidade com as variáveis adicionadas, assim o erro de tipagem tinha que alterado pelo prórprio usuário. Solucionei parcialmente, já que a interface desta parte do programa está ruim, ainda têm esse erro com as porcentagens que utilizam o símbolo "%" no seu final e a planilha está sendo salva de uma forma errada, tal qual o usuário deve restaurá-la sempre depois de uma alteração feita pelo programa.

--- InvestSheet_teste.py
+++ InvestSheet_teste.py
@@ -6,7 +6,7 @@ acs = []
 
 mod = open("moderador.txt", "r")
 
-def atualizar():
+def atualizar_predef():
     mod = open("moderador.txt", "r")
     for i in mod:
         coluna, acao = i.split(";")
@@ -15,18 +15,18 @@ def atualizar():
             "ac": acao,
         })
 
-def mostrar():
+def mostrar_predef():
     print("Sua predefinição de ações:\n")
     for a in acs:
         print(a["col"], a["ac"], sep=", ")
 
-def adicionar():
+def adicionar_predef():
     ac = input("Você quer adicionar qual ação?\n")
     col = input("\nEm qual coluna está essa ação?\n")
     mod = open("moderador.txt", "a")
     mod.write(f"{col.capitalize().strip()};{ac.strip()}")
 
-def procurar(coluna2, ac2):
+def procurar_ac(coluna2, ac2):
     chrome = webdriver.Chrome()
     chrome.get(f'https://statusinvest.com.br/acoes/{ac2}')
     preco = chrome.find_element(
@@ -46,18 +46,58 @@ def procurar(coluna2, ac2):
     cagrL = chrome.find_element(
         By.XPATH, '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[2]/div/div/strong').text
     cagrR = chrome.find_element(
-        By.XPATH, '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[1]/div/div/strong').text
+        By.XPATH, '//*[@id="payout-section"]/div/div/div[1]/div[1]/div[1]/strong').text
 
-    tab2[f'{coluna2}1'] = ac2
-    tab2[f'{coluna2}3'] = preco
-    tab2[f'{coluna2}4'] = roe
-    tab2[f'{coluna2}5'] = dy
-    tab2[f'{coluna2}6'] = div_ebitda
-    tab2[f'{coluna2}7'] = pl
-    tab2[f'{coluna2}8'] = pvp
-    tab2[f'{coluna2}9'] = payout
-    tab2[f'{coluna2}10'] = cagrL
-    tab2[f'{coluna2}11'] = cagrR
+    # o número não conseguia ser convertido em float, pois tinha vírgula, e o float utiliza na numeração americana
+    tab2[f'{coluna2}1'] = str(ac2.upper())
+    tab2[f'{coluna2}3'] = float(preco.replace(",","."))
+    try:
+        tab2[f'{coluna2}4'] = float(roe.replace("%","").replace(",","."))
+    except ValueError:
+        print("\nO valor do ROE não está disponível.")
+        tab2[f'{coluna2}4'] = "-"
+
+    try:
+        tab2[f'{coluna2}5'] = float(dy.replace(",","."))
+    except ValueError:
+        print("\nO valor de Dividend Yield não está disponível.")
+        tab2[f'{coluna2}5'] = "-"
+
+    try:
+        tab2[f'{coluna2}6'] = float(div_ebitda.replace(",","."))
+    except ValueError:
+        print("\nO valor de Dívida Líquida/ EBITDA não está disponível.")
+        tab2[f'{coluna2}6'] = "-"
+    
+    try:
+        tab2[f'{coluna2}7'] = float(pl.replace(",","."))
+    except:
+        print("\nO valor do PL não está disponível.")
+        tab2[f'{coluna2}7'] = "-"
+
+    try:
+        tab2[f'{coluna2}8'] = float(pvp.replace(",","."))
+    except ValueError:
+        print("\nO valor do PVP não está disponível.")
+        tab2[f'{coluna2}8'] = "-"
+    
+    try:
+        tab2[f'{coluna2}9'] = float(payout.replace("%",".").replace(",","."))
+    except ValueError:
+        print("\nO valor do PAYOUT não está disponível.")
+        tab2[f'{coluna2}9'] = "-"
+    
+    try:
+        tab2[f'{coluna2}10'] = float(cagrL.replace("%","").replace(",","."))
+    except ValueError:
+        print("\nO valor de CAGR do Lucro não está disponível.")
+        tab2[f'{coluna2}10'] = "-"
+    
+    try:
+        tab2[f'{coluna2}11'] = float(cagrR.replace("%","").replace(",","."))
+    except ValueError:
+        print("\nO valor de CAGR dee Receita não está disponível.")
+        tab2[f'{coluna2}11'] = "-"
 
 def main1():
 
@@ -75,19 +115,19 @@ def main1():
         if sob == 1:
             break
 
-    procurar(coluna, ac)
+    procurar_ac(coluna, ac)
 
 def main2():
     while True:
         p1 = int(input("[1] Mostrar predefinição\n[2] Adicionar ação\n[3] Preencher planilha\n[4] Sair\n"))
         if p1 == 1:
-            mostrar()
+            mostrar_predef()
         if p1 == 2:
-            adicionar()
-            atualizar()
+            adicionar_predef()
+            atualizar_predef()
         if p1 == 3:
             for n in acs:
-                procurar(n["col"], n["ac"])
+                procurar_ac(n["col"], n["ac"])
         if p1 == 4:
             exit()
 
